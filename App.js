d2bf76bcedb0d86d5d8b36887a89f65dfa025f82
dm25an22 <dm25an22@gmail.com>
2021-03-02 16:30:54 +0100

    add api

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ScrollView, StyleSheet, View } from 'react-native';
+import Api from './api';
 import Header from './components/header';
 import Info from './components/info';
 import Week from './components/week';
 
 export default function App() {
+
+  
+async function fetchData() {
+  const data = await Api.fetchForecastData();
+}
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Header />
---
+++ api.js
@@ -0,0 +1,32 @@
+export default class Api {
+  static _checkStatus = async (response) => {
+    if (response.status >= 200 && response.status < 300) {
+      return await response.json();
+    }
+    throw new Error(`${response.status}: ${response.statusText}`);
+  }
+
+  static async _apiGET(url, headers) {
+    console.log("ok")
+    try {
+      const response = await fetch(url, {
+        "method": "GET",
+        headers
+      });
+      return this._checkStatus(response);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async fetchForecastData() {
+    try {
+      return await this._apiGET(`https://community-open-weather-map.p.rapidapi.com/forecast?q=san%20francisco%2Cus`, {
+        "x-rapidapi-key": "0861a540bbmsh9ef63039d5fd203p1883bejsn220d1c3c7cd8",
+        "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
+      });
+    } catch (error) {
+      throw error
+    }
+  }
+}
